3195833e8d583e07fd236aa7dec53732b49f75db
Aldiansyah Dwi P <aldiansyhdp@gmail.com>
2023-04-18 11:57:20 +0700

    finish sign up logic

---
+++ prisma/migrations/20230418043007_add_tasks_and_relation/migration.sql
@@ -0,0 +1,37 @@
+/*
+  Warnings:
+
+  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "User";
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "username" TEXT NOT NULL,
+    "hash_password" TEXT NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "tasks" (
+    "id" SERIAL NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "title" TEXT NOT NULL,
+    "done" BOOLEAN NOT NULL,
+    "userId" INTEGER,
+
+    CONSTRAINT "tasks_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_username_key" ON "users"("username");
+
+-- AddForeignKey
+ALTER TABLE "tasks" ADD CONSTRAINT "tasks_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { AuthDto } from './dto/auth.dto';
 import { PrismaService } from '../prisma/prisma.service';
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class AuthService {
@@ -9,17 +10,27 @@ export class AuthService {
 
   async signUp(authDto: AuthDto) {
     const hash = await argon.hash(authDto.password);
-    const user = await this.prisma.user.create({
-      data: {
-        username: authDto.username,
-        hash_password: hash,
-      },
-      select: {
-        id: true,
-        username: true,
-      },
-    });
 
-    return user;
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          username: authDto.username,
+          hash_password: hash,
+        },
+        select: {
+          id: true,
+          username: true,
+        },
+      });
+
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credential taken');
+        }
+        throw error;
+      }
+    }
   }
 }
