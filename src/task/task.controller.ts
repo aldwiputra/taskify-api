8771de619c3352563c4b2d6fcf8343eec593fd5f
Aldiansyah Dwi P <aldiansyhdp@gmail.com>
2023-04-20 11:13:41 +0700

    add getAllTasks

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { AuthenticatedReq } from 'src/user/user.controller';
+import { AuthGuard } from '@nestjs/passport';
 
-@Controller('task')
-export class TaskController {}
+@UseGuards(AuthGuard('jwt'))
+@Controller('tasks')
+export class TaskController {
+  constructor(private prisma: PrismaService) {}
+
+  @Get()
+  getAllTasks(@Req() req: AuthenticatedReq) {
+    const user = this.prisma.task.findMany();
+
+    return user;
+  }
+}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,11 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { AuthGuard } from '@nestjs/passport';
+import { User } from '@prisma/client';
+
+export interface AuthenticatedReq extends Request {
+  user: User;
+}
 
 @UseGuards(AuthGuard('jwt'))
 @Controller('users')
@@ -8,7 +13,7 @@ export class UserController {
   constructor(private userService: UserService) {}
 
   @Get('me')
-  getUser(@Req() req) {
+  getUser(@Req() req: AuthenticatedReq) {
     return this.userService.getUser(req.user);
   }
 }
